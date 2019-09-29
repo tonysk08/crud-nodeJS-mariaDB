c3301cadd388d6e15aec96492ec4e2b2ef7735cf
Tonysk08 <zarmiento227@gmail.com>
2019-09-29 16:14:40 -0500

    scrip de vase de datos definitivo

--- src/database/db.sql
+++ src/database/db.sql
@@ -1,10 +1,34 @@
-CREATE TABLE `customers` (
-	`id` INT(6) NOT NULL AUTO_INCREMENT,
-	`Name` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8_spanish_ci',
-	`address` VARCHAR(100) NOT NULL DEFAULT '0' COLLATE 'utf8_spanish_ci',
-	`phone` VARCHAR(15) NOT NULL DEFAULT '0' COLLATE 'utf8_spanish_ci',
-	PRIMARY KEY (`id`)
-)
-COLLATE='utf8_spanish_ci'
-ENGINE=InnoDB
-;
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.3.16-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Volcando estructura de base de datos para crudnodejs
+CREATE DATABASE IF NOT EXISTS `crudnodejs` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
+USE `crudnodejs`;
+
+-- Volcando estructura para tabla crudnodejs.customers
+CREATE TABLE IF NOT EXISTS `customers` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) COLLATE utf8_spanish_ci NOT NULL DEFAULT '0',
+  `address` varchar(100) COLLATE utf8_spanish_ci NOT NULL DEFAULT '0',
+  `phone` varchar(15) COLLATE utf8_spanish_ci NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- Volcando datos para la tabla crudnodejs.customers: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
