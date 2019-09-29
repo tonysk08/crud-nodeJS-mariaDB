c5111a76780435188fdf0ed1d2b173f2b1818812
Tonysk08 <zarmiento227@gmail.com>
2019-09-29 17:24:14 -0500

    creacion de los controles y rutas basicas para las cosultas

---
+++ src/controllers/customerController.js
@@ -0,0 +1,21 @@
+const controller = {};
+
+controller.list = (req, res) => {
+    req.getConnection((err,conn) => {
+        conn.query('SELECT * FROM customers', (err,customers) =>{
+            if(err)
+            {
+                res.json(err);
+            }
+            res.render('')
+        });
+    });
+};
+
+
+controller.save = (req, res) => {res.send('save')};
+controller.delete = (req, res) => {res.send('delete')};
+controller.update = (req, res) => {res.send('update')};
+controller.test = (req, res) => {res.send('hello World')};
+
+module.exports = controller
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const customersController = require('../controllers/customerController')
 
 const router = express.Router();
+
+router.get('/',);
 
 module.exports = router;
