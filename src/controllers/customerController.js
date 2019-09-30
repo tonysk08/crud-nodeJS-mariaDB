c998ba4f2996142dcceefb0cf939c4ed352deb95
Tonysk08 <zarmiento227@gmail.com>
2019-09-30 02:47:20 -0500

    se agrega las rutas para los contoles,  los metodos  qwue permineten la interaccion

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -7,15 +7,54 @@ controller.list = (req, res) => {
             {
                 res.json(err);
             }
-            res.render('customers',)
+            //console.log(customers)
+            res.render('customers',{
+                data:customers
+            })
         });
     });
 };
 
 
-controller.save = (req, res) => {res.send('save')};
-controller.delete = (req, res) => {res.send('delete')};
-controller.update = (req, res) => {res.send('update')};
+controller.save = (req, res) => {
+    const data = req.body;
+    req.getConnection((err,conn) => {
+        conn.query('INSERT INTO customers set ?',[data], (err,customers)=>{
+            //console.log(customers);
+            res.redirect('/');
+        });
+    })
+};
+controller.delete = (req, res) => {
+    //console.log(req.params.id);
+
+    const id = req.params.id;
+    req.getConnection((err, conn) => {
+        conn.query('DELETE FROM customers WHERE id = ?',[id], (err, rows) =>{
+            res.redirect('/');
+        });
+    })
+};
+controller.edit = (req, res) => {
+    const id = req.params.id;
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM customers WHERE id = ?',[id], (err, customers) =>{
+            res.render('customer_edit',{
+                data:customers[0]
+            });
+        });
+    });
+};
+controller.update = (req, res) => {
+    const id = req.params.id;
+    const newCustomer = req.body;
+    req.getConnection((err, conn) => {
+        conn.query('UPDATE customers SET ? WHERE id = ?',[newCustomer,id], (err, rows) =>{
+            res.redirect('/');
+        });
+    });
+};
+
 controller.test = (req, res) => {res.send('hello World')};
 
 module.exports = controller
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -3,6 +3,10 @@ const customersController = require('../controllers/customerController')
 
 const router = express.Router();
 
-router.get('/',);
+router.get('/',customersController.list);
+router.post('/add',customersController.save)
+router.get('/delete/:id',customersController.delete)
+router.get('/update/:id', customersController.edit)
+router.post('/update/:id', customersController.update)
 
 module.exports = router;
